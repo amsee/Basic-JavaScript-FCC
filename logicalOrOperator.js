529ded0d3fd8efae1790f8a1b5f11746f14e8971
Amy Wong <sn0w@Amy-Wong.local>
2018-04-12 11:59:02 -0400

    Comparisons with the Logical Or Operator

--- logicalOrOperator.js
+++ logicalOrOperator.js
@@ -11,20 +11,21 @@
 // if (num > 10 || num < 5) {
 //   return "No";
 // }
-return "Yes";
+// return "Yes";
 
 
 function testLogicalOr(val) {
-  // Only change code below this line
-
-  if (val > 20 || val < 10) {
-    return "Outside";
+    // Only change code below this line
+  
+    if (val > 20 || val < 10) {
+      return "Outside";
+    }
+  
+  
+    // Only change code above this line
+    return "Inside";
   }
-
-
-  // Only change code above this line
-  return "Inside";
-}
-
-// Change this value to test
-testLogicalOr(15);
+  
+  // Change this value to test
+  testLogicalOr(15);
+  
