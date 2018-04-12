6af7c848c5e6f6fe59ed335494ccac76938ee740
Amy Wong <sn0w@Amy-Wong.local>
2018-04-12 10:22:28 -0400

    Comparisons with the Logical And Operator

---
+++ logicalAndOperator.js
@@ -0,0 +1,29 @@
+// if (num > 5) {
+//   if (num < 10) {
+//     return "Yes";
+//   }
+// }
+// return "No";
+
+// OR
+
+// if (num > 5 && num < 10) {
+//   return "Yes";
+// }
+// return "No";
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val > 24 && val < 51) {
+        return "Yes";
+      
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  // Change this value to test
+  testLogicalAnd(10);
+  
