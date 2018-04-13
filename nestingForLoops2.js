68c96fcfaf235653dad2883fb9a01455991dbb1e
Amy Wong <sn0w@Amy-Wong.local>
2018-04-13 14:37:54 -0400

    Nesting For Loops

--- nestingForLoops2.js
+++ nestingForLoops2.js
@@ -1,11 +1,23 @@
+// i:  0
+// i: 0       j: 0
+// i: 0       j: 1
+// i:  1
+// i: 1       j: 0
+// i: 1       j: 1
+// i:  2
+// i: 2       j: 0
+// i: 2       j: 1
+// i: 2       j: 2
+// => NaN
+
 
 function multiplyAll(arr) {
   var product = 1;
 
-  // 1st dimension of array
+  // * 1st dimension of array
   for (var i=0; i < arr.length; i++) { // 3 arrays
   
-    // 2nd dimension - arrays inside the big array
+    // * 2nd dimension - arrays inside the big array
     console.log("i: ", i);
     
     for (var j=0; j < arr[i].length; j++)
@@ -16,10 +28,13 @@ function multiplyAll(arr) {
   return product;
 }
 
-// Modify values below to test your code
+// *Modify values below to test your code
 var arr = [
   [1,2],
   [3,4],
   [5,6,7]
 ];
 multiplyAll(arr);
+
+
+
