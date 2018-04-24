ed1735487ca40c22ba4b3276d3fe63531bc81726
Amy Wong <sn0w@eduroam-198-96-32-19.eduroam.yorku.ca>
2018-04-24 11:24:45 -0400

    C++ and JavaScript Comparisons

---
+++ cppJavaScriptComparisons.js
@@ -0,0 +1,38 @@
+// C++
+class Car
+{
+  int speed = 10;
+  
+  public:
+    void accelerate(change)
+    {
+      speed += change;
+    }
+    void decelerate()
+    {
+      speed -= 5;
+    }
+    int getSpeed()
+    {
+      return speed;
+    }
+};
+
+// JavaScript
+var Car = function() {
+  // this is a private variable
+  var speed = 10;
+
+  // these are public methods
+  this.accelerate = function(change) {
+    speed += change;
+  };
+
+  this.decelerate = function() {
+    speed -= 5;
+  };
+
+  this.getSpeed = function() {
+    return speed;
+  };
+};
